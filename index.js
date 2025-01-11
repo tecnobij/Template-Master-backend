9df7be290730028a9c02878e8b67de4698b7a1f6
tecnobij <official.tecnobij@gmail.com>
2025-01-11 13:05:14 +0530

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-// Import required modules
 require('dotenv').config();
-const app = require('./app'); // Import app from app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const userRoute = require('./src/routes/user.router');
+const adminRoute = require('./src/routes/admin.routes');
+const uploadImages = require('./src/routes/image.routes');
+const uploadVideo = require('./src/routes/video.routes');
+const tagRoute = require('./src/routes/tag.routes');
 const connectToMongoDB = require('./src/config/db');
 
-// Initialize and configure environment variables
+const app = express();
+const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:5173"];
+const PORT = process.env.PORT || 3000;
+
 connectToMongoDB();
 
-console.log('MongoDB connection initialized successfully.');
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use('/api/V1/user', userRoute);
+app.use('/api/V1/admin', adminRoute);
+app.use('/api/V1/images', uploadImages);
+app.use('/api/V1/video', uploadVideo);
+app.use('/api/V1/tag', tagRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
